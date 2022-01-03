5613a8cecd71313009aa38223db82f2039d8a0c3
Filipe <filipelehmannp@gmail.com>
2022-01-03 11:05:37 -0300

    Customization of Call to Action's panels, sections and fields

--- bootstrap2wordpress/includes/customizer-b2w.php
+++ bootstrap2wordpress/includes/customizer-b2w.php
@@ -129,10 +129,10 @@ Kirki::add_field(
 		'type'     => 'text',
 		'settings' => 'footer_sub_heading',
 		'label'    => esc_html__('Sub Heading', 'bootstrap2wordpress'),
-		'tooltip'  => esc_html__('Call to Action Section', 'bootstrap2wordpress'),
+		'tooltip'  => esc_html__('Call to action section', 'bootstrap2wordpress'),
 		'section'  => 'footer_calltoaction_section',
 		'default'  => esc_html__('Join the newsletter', 'bootstrap2wordpress'),
-		'partial-refres' => array(
+		'partial_refresh' => array(
 			'footer_sub_heading' => array(
 				'selector'	=> '.footer-calltoaction p.sub-title',
 				'render_callback'	=> function () {
@@ -140,5 +140,85 @@ Kirki::add_field(
 				}
 			)
 		)
+	)
+);
+
+Kirki::add_field(
+	'b2w_customizer_config',
+	array(
+		'type'     				=> 'text',
+		'settings' 				=> 'footer_calltoaction_heading',
+		'label'    				=> esc_html__('Heading', 'bootstrap2wordpress'),
+		'tooltip'  				=> esc_html__('Enter the call to action heading text', 'bootstrap2wordpress'),
+		'section'	 				=>	'footer_calltoaction_section',
+		'default'  				=> esc_html__('Bootstrap To Wordpress', 'bootstrap2wordpress'),
+		'partial_refresh'	=>	array(
+			'footer_calltoaction_heading'	=>	[
+				'selector'	=>	'.footer-heading',
+				'render_callback'	=> function () {
+					return get_theme_mod('footer_calltoaction_heading');
+				}
+			]
+		)
+	)
+);
+
+Kirki::add_field(
+	'b2w_customizer_config',
+	array(
+		'type'						=>	'textarea',
+		'settings'				=>	'footer_calltoaction_desc',
+		'label'						=>	esc_html__('Description', 'bootstrap2wordpress'),
+		'tooltip'					=>	esc_html__('Enter the call to action description', 'bootstrap2wordpress'),
+		'section'					=>	'footer_calltoaction_section',
+		'default'					=>	esc_html__('Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis tempora distinctio iusto modi laborum veniam unde doloribus inventore. Quisquam rerum nisi a ut quae nulla aspernatur? Eaque ea atque unde.', 'bootstrap2wordpress'),
+		'partial_refresh'	=>	array(
+			'footer_calltoaction_desc'	=>	[
+				'selector'		=>	'.footer-calltoaction p.ftca-desc',
+				'render_callback'	=>	function () {
+					return get_theme_mod('footer_calltoaction_desc');
+				}
+			]
+		)
+	)
+);
+
+Kirki::add_field(
+	'b2w_customizer_config',
+	array(
+		'type'						=>	'textarea',
+		'settings'				=>	'footer_calltoaction_btn',
+		'label'						=>	esc_html__('Button', 'bootstrap2wordpress'),
+		'tooltip'					=>	esc_html__('Enter the button message', 'bootstrap2wordpress'),
+		'section'					=>	'footer_calltoaction_section',
+		'default'					=>	esc_html__('Join now ->'),
+		'partial_refresh'	=>	array(
+			'footer_calltoaction_btn'	=>	array(
+				'selector'		=>	'.footer-calltoaction .btn',
+				'render_callback'	=>	function () {
+					return get_theme_mod('footer_calltoaction_btn');
+				}
+			)
+		)
+	)
+);
+
+Kirki::add_field(
+	'b2w_customizer_config',
+	array(
+		'type'						=>	'link',
+		'settings'				=>	'footer_cta_link',
+		'label'						=>	esc_html__('Button link', 'bootstrap2wordpress'),
+		'tooltip'					=>	esc_html__('Enter a valid url here', 'bootstrap2wordpress'),
+		'section'					=>	'footer_calltoaction_section',
+		'default'					=>	'#',
+		// 'partial_refresh'	=>	array(
+		// 	'footer_calltoaction_btn'	=>	array(
+		// 		'selector'		=>	'.footer-calltoaction .btn',
+		// 		'render_callback'	=>	function () {
+		// 			return get_theme_mod('footer_calltoaction_btn');
+		// 		}
+		// 	)
+		// )
 	)
 );
